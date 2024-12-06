f7f2d7ff51018785e022a620f47c87757af2ac70
Walle <mohouali.748@gmail.com>
2024-12-06 17:53:08 +0000

    change the way to handle dynamic properties from variable injection to searchable variable with prefix

--- src/main/java/org/sid/billing/ConsulConfigRestController.java
+++ src/main/java/org/sid/billing/ConsulConfigRestController.java
@@ -1,5 +1,6 @@
 package org.sid.billing;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RefreshScope
+//@RefreshScope <- old not needed with ConfigurationProperties
 public class ConsulConfigRestController {
-    @Value("${token.accessTokenTimeout}")
-    private long accessTokenTimeout;
-    @Value("${token.refreshTokenTimeout}")
-    private long refreshTokenTimeout;
+
+//    old way with injection
+
+//    @Value("${token.accessTokenTimeout}")
+//    private long accessTokenTimeout;
+//    @Value("${token.refreshTokenTimeout}")
+//    private long refreshTokenTimeout;
+//    @GetMapping("/myConfig")
+//    public Map<String, Object> myConfig() {
+//        return Map.of("accessTokenTimeout",accessTokenTimeout, "refreshTokenTimeout",refreshTokenTimeout);
+//    }
+    // new way with ConfigurationProperties
+    @Autowired
+    private MyConsulConfig myConsulConfig;
+
     @GetMapping("/myConfig")
-    public Map<String, Object> myConfig() {
-        return Map.of("accessTokenTimeout",accessTokenTimeout, "refreshTokenTimeout",refreshTokenTimeout);
+    public MyConsulConfig myConfig() {
+        return myConsulConfig;
     }
+
 }
